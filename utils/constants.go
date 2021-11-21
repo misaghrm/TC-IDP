368887da43179a7387e4e26daa3db697cba36998
misaghrm <50659555+misaghrm@users.noreply.github.com>
2021-11-21 19:50:21 +0330

    Update constants.go
    just for test

--- utils/constants.go
+++ utils/constants.go
@@ -36,10 +36,10 @@ const (
 	RefreshTokens    = "RefreshTokens"
 	ClientID         = "ClientID"
 
-	PgUrlDev      = "host=10.1.10.69 user=m_ramezani password=S7B-C=aUzt7h@HCv dbname=auth port=54327 sslmode=disable"
-	PgUrlTest     = "host=10.1.10.59 user=m_ramezani password=j3cNeRarL72?D$nd*X%Z dbname=auth port=54327 sslmode=disable"
-	PgUrlProd     = "host=10.1.10.105 user=m_ramezani password=2?BA5+H6jzmKL*q2%s*cqL2u_RXR dbname=auth port=54327 sslmode=disable"
-	PgUrlLocal    = "postgresql://Misagh:13750620@127.0.0.1:5432/tazminchi"
+	PgUrlDev      = ""
+	PgUrlTest     = ""
+	PgUrlProd     = ""
+	PgUrlLocal    = ""
 	EncryptingKey = `"EncryptingKey"`
 	SigningKey    = `"SigningKey"`
 	Issuer        = `"Issuer"`
@@ -56,9 +56,9 @@ const (
 	IssuerHeader  = "tc:iss"
 	EulaVersion   = "1"
 
-	DomainProd = "https://napi.tazminchi.com/"
-	DomainDev  = "https://dev.tazminchi.com/"
-	DomainTest = "https://test.tazminchi.com/"
+	DomainProd = ""
+	DomainDev  = ""
+	DomainTest = ""
 
 	EncryptingKeyIndex = 0
 	SigningKeyIndex    = 1
